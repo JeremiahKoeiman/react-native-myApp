cb0f24f18cc8e9a79c14f27f532275e81e02af12
Jeremiah Koeiman <9001603@student.zadkine.nl>
2021-04-22 11:27:48 +0200

    Setup project

---
+++ app/screens/Me.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Me = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Me Screen</Text>
+    </View>
+  );
+};
+export default Me;
---
+++ app/screens/NewContact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const NewContact = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>NewContact Screen</Text>
+    </View>
+  );
+};
+export default NewContact;
